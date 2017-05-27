f4389f97bcbf33cec4e909bb8b861206c7b6986c
websterbei <bei.yijie.2014@vjc.sg>
2017-05-27 09:37:13 -0400

    EulerSolver

--- RKOdeSolver.cpp
+++ RKOdeSolver.cpp
@@ -10,12 +10,6 @@ void deri(double dydt[], double y[], double con[]) //dydt[0] y', dydt[1] y'', y[
     return;
 }
 
-double deriv( double y[], double con[]) //dydt[0] y', dydt[1] y'', y[0] old y, y[1] old y'
-{
-   double result= con[0]*y[0]*exp(-(y[0]*y[0]+con[1]*con[1]+con[2]*con[2]));
-    return result;
-}
-
 int main()
 {
     double mass = //Need to specify
@@ -27,7 +21,7 @@ int main()
     double con[3];
     con[0] = 2.0/mass;
     double step = 0.0001;
-    
+
     //Initialize
     double X[11000];
     double Y[11000];
@@ -44,34 +38,17 @@ int main()
 
     while(t<20)
     {
-	//In X direction
+	     //In X direction
         con[1] = initY[0];
         con[2] = initZ[0];
         deri(dydt, initX, con);
-        double k1 = dydt[0]; //k1 for first ODE
-        newX[0] = initX[0] + step/2*k1;
-        double k2 = 
-        k1 = dydt[1]; //k2 for second ODE
-        newX[1] = initX[1] + step/2*k1;
-        
-        
+        double k11 = dydt[0]; //k1 for first ODE
+        newX[0] = initX[0] + step/2*k11; //Update x(t)
+        double k12 = dydt[1]; //k1 for second ODE
+        newX[1] = initX[1] + step/2*k12; //Update x'(t)
+        initX[0] = newX[0];
+        initX[1] = newX[1];
+        deri(dydt, initX, con);
+        double k21 = dydt[0]
     }
-	while(t<20){
-	//In X direction
-        con[1] = initY[0];
-        con[2] = initZ[0];
-        double k1 = initX[1];
-	newX[0]=initX[0]+step*k1/2;
-        double k2 = deriv(newX, con);
-	newX[0]=newX[0]+step*k2/2;
-	double k3 = deriv(newX,con);
-	newX[0]=newX[0]+step*k3;
-	double k4 = deriv(newX,con);
-	initX[0]=initX[0]+step*(k1/6+k2/3+k3/3+k4/6);
-	initX[1]=k4;
-
-	
-	
-	}
-	
 }
