c674f2053d1c1ada5866a20de852c49ac775b1ad
websterbei <bei.yijie.2014@vjc.sg>
2017-06-01 23:29:13 -0400

    Added Kent Sampler

--- initialization.cpp
+++ initialization.cpp
@@ -1,4 +1,5 @@
 #include "particle.h"
+#include "KentSampler.h"
 #include <random>
 #include <fstream>
 #include <cstdlib>
@@ -6,11 +7,31 @@
 #include <cmath>
 using namespace std;
 
+const double mass = 1.0; //Assume mass = 1.0 for now
+const double k = 2.0; //Parameter for kent distribution, assume k = 2
+
 vector<Particle> particles;
 
-normal_distribution<double> getDist(double sigma) //Require the std dev sigma
+double potential(double x, double y, double z) //Potential function
+{
+  return -exp(-(x*x+y*y+z*z));
+}
+
+normal_distribution<double> getNorm(double sigma) //Require the std dev sigma
 {
   normal_distribution<double> distribution(0, sigma);
+  return distribution;
+}
+
+uniform_real_distribution<double> getUnif() //Obtain a uniform real distribution
+{
+  uniform_real_distribution<double> distribution(0.0, 1.0);
+  return distribution;
+}
+
+KentSampler getKent(double x, double y, double z, double k) //k controls the overall angular momentum
+{
+  KentSampler distribution(x, y, z, k); //2 is a suitable number for k
   return distribution;
 }
 
@@ -21,15 +42,22 @@ int main(int argc, char** argv)
   particles.resize(n); //Resize vector to contain the particles
 
   double sigma = ceil(R/sqrt(3));
-  default_random_engine gen; //Random Number generator
-  normal_distribution<double> dist = getDist(sigma); //Normal distribution sampler
+  mt19937 gen; //Random Number generator
+  normal_distribution<double> norm = getNorm(sigma); //Normal distribution sampler
+  uniform_real_distribution<double> unif = getUnif(); //Uniform real distribution sampler
 
   for(vector<Particle>::size_type i=0; i<n; i++)
   {
-    double tmpX = dist(gen);
-    double tmpY = dist(gen);
-    double tmpZ = dist(gen);
-    Particle newParticle(tmpX, tmpY, tmpZ);
+    double tmpX = norm(gen);
+    double tmpY = norm(gen);
+    double tmpZ = norm(gen);
+    double pot = potential(tmpX, tmpY, tmpZ); //Calculate potential energy
+    double maxV = sqrt(-2*pot/mass); //Maximum velocity, assume TE = 0
+    KentSampler kent = getKent(-tmpY, tmpX, 0, k);
+    double tmpV[3];
+    double scaleV = unif(gen);
+    kent.next(tmpV);
+    Particle newParticle(tmpX, tmpY, tmpZ, tmpV[0]*scaleV, tmpV[1]*scaleV, tmpV[2]*scaleV);
     particles[i] = newParticle;
   }
 
@@ -38,7 +66,8 @@ int main(int argc, char** argv)
 
   for(vector<Particle>::size_type i=0; i<n; i++)
   {
-    outputfile<<particles[i].x<<" "<<particles[i].y<<" "<<particles[i].z<<endl;
+    outputfile<<particles[i].x<<" "<<particles[i].y<<" "<<particles[i].z<<" "\
+    <<particles[i].vx<<" "<<particles[i].vy<<" "<<particles[i].vz<<endl;
   }
 
   outputfile.close();
